db3a101c618b158cefb4adca9e2e16383cfcd2b8
auretho <aurelie.thouzeau@gmail.com>
2020-12-23 14:39:31 +0100

    upgrade node-notifier

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -11,7 +11,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <Provider store = {store}>
-    <Router>
+    <Router basename='/'>
       <App />
     </Router>
   </Provider>,
